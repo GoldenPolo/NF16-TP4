65d2860a26e1b720dabb7e25e90445205da9841d
Pol Corty <pol.corty@etu.utc.fr>
2022-05-10 18:27:32 +0200

    5 fini, 4 a faire

--- NF16-TP4/tp4.c
+++ NF16-TP4/tp4.c
@@ -1,1 +1,64 @@
 #include "tp4.h"
+
+
+char * lower_format(char * mot){
+    for ( ; sizeof(mot); ++mot) *mot = tolower(*mot);
+}
+
+int existe_noeud(t_Noeud * noeud, char * mot){
+    mot = lower_format(mot);
+    if (noeud == NULL) {
+        return 0;
+    }
+    else if (noeud->mot == mot) {
+        return 1;
+    }
+    else if (noeud->mot < mot){
+        return existe_noeud(noeud->filsGauche, mot);
+    }
+    else {
+        return existe_noeud(noeud->filsDroit, mot);
+    }
+}
+
+
+int ajouter_noeud(t_Index * index, t_Noeud * noeud){
+    
+    if (existe_noeud(index->racine, noeud->mot) == 1) {
+        // noeud existe déjà
+        return 0;
+    }
+    else {
+        //si noeud a rajouter
+        if (index->racine == NULL) {
+            index->racine = noeud;
+        }
+        else {
+            
+            t_Noeud * actuel = index->racine;
+            t_Noeud * pere = NULL;
+            int derniere_action;
+            
+            while (actuel != NULL) {
+                if (actuel->mot < noeud->mot) {
+                    pere = actuel;
+                    actuel = actuel->filsDroit;
+                    derniere_action = 1;
+                }
+                else {
+                    pere = actuel;
+                    actuel = actuel->filsGauche;
+                    derniere_action = 0;
+                }
+            }
+            if (derniere_action == 1) {
+                pere->filsDroit = noeud;
+            }
+            else {
+                pere->filsGauche = noeud;
+            }
+        }
+        
+        
+    }
+}
--- NF16-TP4/tp4.h
+++ NF16-TP4/tp4.h
@@ -1,46 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 typedef struct t_position {
     int numero_ligne;
     int ordre;
     int numero_phrase;
     struct t_position * suivant;
-} t_position;
+} t_Position;
 
 typedef struct t_listePositions {
-    t_position * debut;
+    t_Position * debut;
     int nb_elements;
-} t_listePositions;
+} t_ListePositions;
 
 typedef struct t_noeud {
     char * mot;
     int nb_occurences;
-    t_listePositions positions;
+    t_ListePositions positions;
     struct t_noeud * filsGauche;
     struct t_noeud * filsDroit;
-} t_noeud;
+} t_Noeud;
 
 typedef struct t_index {
-    t_noeud * racine;
+    t_Noeud * racine;
     int nb_mots_differents;
     int nb_mots_total;
-} t_index;
+} t_Index;
 
-t_listePositions * creer_liste_positions();
+t_ListePositions * creer_liste_positions();
 
-int ajouter_position(t_listePositions * listeP, int ligne, int ordre, int num_phrase);
+int ajouter_position(t_ListePositions * listeP, int ligne, int ordre, int num_phrase);
 
-t_index * creer_index();
+t_Index * creer_index();
 
-t_noeud * rechercher_mot(t_index * index, char * mot);
+t_Noeud * rechercher_mot(t_Index * index, char * mot);
 
-int ajouter_noeud(t_index * index, t_noeud * noeud);
+int ajouter_noeud(t_Index * index, t_Noeud * noeud);
 
-int indexer_fichier(t_index * index, char * filename);
+int indexer_fichier(t_Index * index, char * filename);
 
-void afficher_index(t_index * index);
+void afficher_index(t_Index * index);
 
-void afficher_max_apparition(t_index * index);
+void afficher_max_apparition(t_Index * index);
 
-void afficher_occurences_mot(t_index * index, char * mot);
+void afficher_occurences_mot(t_Index * index, char * mot);
 
-void construire_texte(t_index * index, char * filename);
+void construire_texte(t_Index * index, char * filename);
+
+//ajouts
+
+char * lower_format(char * mot);
+
+int existe_noeud(t_Noeud * noeud, char * mot);
