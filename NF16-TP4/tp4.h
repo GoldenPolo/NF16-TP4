55c3d2b26975bba8fb40c5f2bfa0dbecfa49065d
freebird1000 <105301697+freebird1000@users.noreply.github.com>
2022-05-30 19:37:40 +0200

    Version complète sauf Q10

--- NF16-TP4/tp4.h
+++ NF16-TP4/tp4.h
@@ -1,46 +1,54 @@
+#ifndef TP4_TP4_H
+#define TP4_TP4_H
+#endif //TP4_TP4_H
+#define TAILLE_MAX 1000
 
 typedef struct t_position {
     int numero_ligne;
     int ordre;
     int numero_phrase;
+    int placement_phrase;
     struct t_position * suivant;
-} t_position;
+} t_Position;
 
 typedef struct t_listePositions {
-    t_position * debut;
+    t_Position * debut;
     int nb_elements;
-} t_listePositions;
+} t_ListePositions;
 
 typedef struct t_noeud {
     char * mot;
     int nb_occurences;
-    t_listePositions positions;
-    struct t_noeud * filsGauche;
-    struct t_noeud * filsDroit;
-} t_noeud;
+    t_ListePositions positions;
+    struct t_index **filsGauche;
+    struct t_index **filsDroit;
+} t_Noeud;
 
 typedef struct t_index {
-    t_noeud * racine;
+    t_Noeud * racine;
     int nb_mots_differents;
     int nb_mots_total;
-} t_index;
+} t_Index;
 
-t_listePositions * creer_liste_positions();
+t_ListePositions * creer_liste_positions();
 
-int ajouter_position(t_listePositions * listeP, int ligne, int ordre, int num_phrase);
+int ajouter_position(t_ListePositions * listeP, int ligne, int ordre, int num_phrase, int placement_phrase);
 
-t_index * creer_index();
+t_Index * creer_index();
 
-t_noeud * rechercher_mot(t_index * index, char * mot);
+t_Noeud * rechercher_mot(t_Index * index, char * mot);
 
-int ajouter_noeud(t_index * index, t_noeud * noeud);
+int ajouter_noeud(t_Index * index, t_Noeud * noeud);
 
-int indexer_fichier(t_index * index, char * filename);
+int indexer_fichier(t_Index * index, char * filename);
 
-void afficher_index(t_index * index);
+void afficher_index(t_Index * index);
 
-void afficher_max_apparition(t_index * index);
+void afficher_max_apparition(t_Index * index);
 
-void afficher_occurences_mot(t_index * index, char * mot);
+void afficher_occurrences_mot(t_Index * index, char * mot);
 
-void construire_texte(t_index * index, char * filename);
+void construire_texte(t_Index * index, char * filename);
+
+// rajouté
+int parcours_infixe(t_Noeud * noeud, t_Noeud * tableau[], int i);
